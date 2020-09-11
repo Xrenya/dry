559b0085852493e07f83d35fe08945b1b1035bab
Rinat <51479797+Xrenya@users.noreply.github.com>
2020-09-11 18:00:50 +0900

    Create save_load_pickle.py

---
+++ save_load_pickle.py
@@ -0,0 +1,7 @@
+def save_obj(obj, filename):
+  with open(f"{filename}.pkl", "wb") as f:
+    pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+  with open(f"{name}.pkl", "rb") as f:
+    return pickle.load(f)
