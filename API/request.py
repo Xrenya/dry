b90e74b36d910350fc0425e081b410e09545aac9
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-19 18:05:21 +0900

    Create request.py

---
+++ API/request.py
@@ -0,0 +1,14 @@
+import requests
+import json 
+import numpy as np
+import cv2
+
+url = "http://127.0.0.1:8080/files"
+
+multiple_files = [
+        ('image1', ('image.png', open('image.png', 'rb'), 'image/png')),
+        ('image2', ('image.png', open('image.png', 'rb'), 'image/png'))
+]
+
+r = requests.post(url, files=multiple_files)
+print(r.text)
